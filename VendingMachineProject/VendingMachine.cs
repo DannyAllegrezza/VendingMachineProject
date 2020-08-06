3faeaf087cebe70fca544c61c4cfc37c583ce479
Danny Allegrezza <danny@dannyallegrezza.com>
2020-08-06 19:19:44 -0400

    Implement super rough first pass of logic, add test cases
    * Added a pretty brute method around determining the proper amount of
    change to return
    * Added additional properties to class to hold things like the amount to
    return and currently inserted change
    * I realize there is a lot of work left to be done, but I just wanted to
    have _something_ to submit to you both!

--- VendingMachineProject/VendingMachine.cs
+++ VendingMachineProject/VendingMachine.cs
@@ -1,42 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace VendingMachineProject
 {
     public class VendingMachine
     {
-        private double SNACK_PRICE = 100;
-        private Stack<Snack> _inventory { get; set; }
-        private List<Coin> _currentlyInsertedCoins { get; set; }
+        private int SNACK_PRICE = 100;
+        private Stack<Snack> _inventory { get; set; } = new Stack<Snack>();
+        private List<Coin> _dimesInMachine { get; set; } = new List<Coin>();
+        private List<Coin> _nickelsInMachine { get; set; } = new List<Coin>();
+
+        private static Coin Dime = new Coin(10);
+        private static Coin Nickel = new Coin(5);
+
         private int _insertedChange { get; set; }
+        private int _amountToReturn { get; set; }
 
         public VendingMachine(List<Snack> inventory)
         {
-            SetupSnackmachine();
             FillInventory(inventory);
         }
 
-        private void SetupSnackmachine()
-        {
-            _inventory = new Stack<Snack>();
-            _currentlyInsertedCoins = new List<Coin>();
-        }
-
-        private void FillInventory(List<Snack> inventory)
-        {
-            if (inventory == null || inventory.Count == 0)
-            {
-                throw new ArgumentNullException("inventory");
-            }
-            inventory.ForEach(x => _inventory.Push(x));
-        }
-
+        /// <summary>
+        /// Get a snack from our vending machine!
+        /// </summary>
+        /// <returns>Right now we only serve Butterfinger bars :D</returns>
         public Snack GetSnack()
         {
             if (_insertedChange >= SNACK_PRICE && _inventory.Peek() != null)
             {
+                _amountToReturn = (_insertedChange - SNACK_PRICE);
                 return _inventory.Pop();
             }
             else
@@ -48,20 +41,67 @@ namespace VendingMachineProject
         /// <summary>
         /// Returns any left over change in the fewest number of appropriate coins.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Any left over change, using the fewest number of appropriate coins.</returns>
         public List<Coin> GetChange()
         {
-            return null;
+            var change = new List<Coin>();
+            
+            if (_amountToReturn > 0)
+            {
+                var possibleDimes = _amountToReturn / 10;
+                for (int i = 0; i < possibleDimes; i++)
+                {
+                    if (_dimesInMachine.Count > 0)
+                    {
+                        _amountToReturn -= Dime.Value;
+                        _dimesInMachine.Remove(Dime);
+
+                        change.Add(Dime);
+                    }
+                }
+
+                var possibleNickels = _amountToReturn / 5;
+                for (int i = 0; i < possibleNickels; i++)
+                {
+                    if (_nickelsInMachine.Count > 0)
+                    {
+                        _amountToReturn -= Nickel.Value;
+                        _dimesInMachine.Remove(Dime);
+
+                        change.Add(Nickel);
+                    }
+                }
+            }
+
+            return change;
         }
 
         public void InsertCoin(Coin coin)
         {
-            // update the currently inserted coins collection           
-            _currentlyInsertedCoins.Add(coin);
+            // update the currently inserted coins collection 
+            if (coin.Value == Dime.Value)
+            {
+                _dimesInMachine.Add(Dime);
+            } 
+            else if (coin.Value == Nickel.Value) 
+            {
+                _nickelsInMachine.Add(Nickel);
+            }
+
             _insertedChange += coin.Value;
+            _amountToReturn += coin.Value;
 
             // TODO: validate the coin
             // TODO: handle invalid currency?
+        }
+
+        private void FillInventory(List<Snack> inventory)
+        {
+            if (inventory == null || inventory.Count == 0)
+            {
+                throw new ArgumentNullException("inventory");
+            }
+            inventory.ForEach(x => _inventory.Push(x));
         }
     }
 }
--- VendingMachineTests/VendingMachineTests.cs
+++ VendingMachineTests/VendingMachineTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using VendingMachineProject;
 
 namespace VendingMachineTests
@@ -36,16 +37,57 @@ namespace VendingMachineTests
         }
 
         [Test]
-
-        public void GetSnack_WhenGivenInsertedAmount_ReturnsExpectedResult()
+        [TestCase(9, 5, 1, 1)]
+        [TestCase(10, 7, 3, 1)]
+        public void GetSnack_WhenGivenInsertedAmount_ReturnsExpectedResult(int insertedDimes, int insertedNickels, int returnedDimes, int returnedNickels)
         {
-            for (int i = 0; i < 10; i++)
+            InsertProvidedCurrency(insertedDimes, insertedNickels);
+
+            var result = _vendingMachine.GetSnack();
+            Assert.AreEqual("Butterfinger", result.Name);
+
+            var returnedChange = _vendingMachine.GetChange();
+            Assert.AreEqual(returnedDimes + returnedNickels, returnedChange.Count);
+
+            Assert.AreEqual(returnedDimes, returnedChange.Where(x => x.Value == 10).Count());
+
+            Assert.AreEqual(returnedNickels, returnedChange.Where(x => x.Value == 5).Count());
+        }
+
+        [Test]
+        public void GetChange_WhenNoChangeIsAvailable_Returns_0_Coins()
+        {
+            InsertProvidedCurrency(0, 0);
+
+            var returnedChange = _vendingMachine.GetChange();
+
+            Assert.AreEqual(0, returnedChange.Count());
+        }
+
+        [Test]
+        public void GetChange_WhenPressedMultipleTimes_Returns_Expected()
+        {
+            InsertProvidedCurrency(insertedDimes: 10, insertedNickels: 4);
+
+            var returnedChange = _vendingMachine.GetChange();
+            var dimesReturned = returnedChange.Where(x => x.Value == 10).Count();
+            var nickelsReturend = returnedChange.Where(x => x.Value == 5).Count();
+
+            Assert.AreEqual(10, dimesReturned);
+            Assert.AreEqual(4, nickelsReturend);
+        }
+
+        private void InsertProvidedCurrency(int insertedDimes, int insertedNickels)
+        {
+            for (int i = 0; i < insertedDimes; i++)
             {
                 _vendingMachine.InsertCoin(new Coin(10));
             }
 
-            var result = _vendingMachine.GetSnack();
-            Assert.AreEqual("Butterfinger", result.Name);
+            for (int i = 0; i < insertedNickels; i++)
+            {
+                _vendingMachine.InsertCoin(new Coin(5));
+            }
         }
     }
 }
